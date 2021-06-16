60e147900e2178964bae53cb676b631eb66575bb
fernandeslizanne <53610361+LizzieFernie@users.noreply.github.com>
2021-06-17 01:41:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Stage from "./Stage/Stage.js";
 import Catcher from "./Catcher/Catcher.js";
 import Baseball from "./Baseball/Baseball.js";
 
-const stage = new Stage({ costumeNumber: 1 ,size: 150});
+let stage = new Stage({ costumeNumber: 1 ,size: 150});
 
 const sprites = {
   Catcher: new Catcher({
